3886ff682d001e4349b70af6764f88592bdb648c
careyarmst <109990504+careyarmst@users.noreply.github.com>
2024-10-31 21:03:04 +0000

    CH2 objectlastex

---
+++ m02testing.js
@@ -0,0 +1,8 @@
+let sculptor = {}
+sculptor = {
+   name: "Michelangelo",
+   artworks: ["David", "Moses", "Bacchus"],
+   bornDate: "March 6, 1475",
+   diedDate: "February 18, 1564",
+ };
+console.log(sculptor.name)
