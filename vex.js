2c2022815ef3ccb49403ad908789cbbf1e89ff77
careyarmst <109990504+careyarmst@users.noreply.github.com>
2024-11-01 03:34:27 +0000

    More practice M02

---
+++ vex.js
@@ -0,0 +1,11 @@
+
+//document.getElementById("Link").href = "https://www.w3.org"
+//let funny = document.querySelectorAll("textarea")
+//console.log(funny)
+
+let funny = document.querySelector("textarea");
+funny.cols = "80";
+console.log(funny)
+
+let vexing2 = document.getElementById("inspiring-quote");
+vexing2.innerHTML ="<strong>Aspire to inspire before we expire.</strong>"
