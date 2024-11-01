49eefaa9fd2bacd2137b61ef8493cf3afc8e3ffa
careyarmst <109990504+careyarmst@users.noreply.github.com>
2024-11-01 18:43:19 +0000

    M02ProjectInitialFiles

---
+++ SDEV255M02EX215/index.js
@@ -0,0 +1,15 @@
+window.addEventListener("DOMContentLoaded", domLoaded);
+
+function domLoaded() {
+   // TODO: Complete the function
+}
+
+function convertCtoF(degreesCelsius) {
+   // TODO: Complete the function
+}
+
+function convertFtoC(degreesFahrenheit) {
+   // TODO: Complete the function
+}
+function displayResults(degreesCelsius, degreesFahrenheit)
+
